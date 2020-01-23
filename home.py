3658a8aad3bc43232e3f55b7861237ee8349ae87
James Hart <james.elliot.hart@gmail.com>
2020-01-23 17:34:27 -0600

    use soup from initialization instead of parameter

--- home.py
+++ home.py
@@ -21,25 +21,26 @@ class Home():
     def _sanitize_home_fact(fact):
         return fact.replace(' ','').replace('/','')
 
-    def _update_price(self, soup):
-        price_raw = soup.find('span', class_='ds-value').text
+    def _update_price(self):
+        price_raw = self.soup.find('span', class_='ds-value').text
         self.info['Price'] = int(price_raw.replace('$','').replace(',',''))
 
-    def _update_address(self, soup):
-        address_raw = soup.find('h1', class_='ds-address-container').text
+    def _update_address(self):
+        address_raw = self.soup.find('h1', class_='ds-address-container').text
         self.info['Address'] = unicodedata.normalize("NFKD", address_raw)
 
-    def _update_bed_bath(self, soup):
-        bed_bath = soup.find('h3', class_='ds-bed-bath-living-area-container').contents
+    def _update_bed_bath(self):
+        bed_bath = self.soup.find('h3', class_='ds-bed-bath-living-area-container').contents
         self.info['Bed'], self.info['Bath'], self.info['Sqft'] = [x.text for x in bed_bath if x.text]
 
-    def update_basic_home_info(self, soup):
-        self._update_price(soup)
-        self._update_address(soup)
-        self._update_bed_bath(soup)
+    def update_basic_home_info(self):
+        self._update_price()
+        self._update_address()
+        self._update_bed_bath()
 
-    def update_home_facts(self, soup):
-        fact_list = soup.find('ul', class_='ds-home-fact-list').contents
+    def update_home_facts(self):
+        # 'Type', 'Yearbuilt', 'Heating', 'Cooling', 'Parking', 'Lot', 'Pricesqft'
+        fact_list = self.soup.find('ul', class_='ds-home-fact-list').contents
         fact_tuples = [fact.text.split(':') for fact in fact_list]
         home_facts = {
             self._sanitize_home_fact(fact_tuple[0]): fact_tuple[1] 
@@ -48,8 +49,8 @@ class Home():
         }
         self.info.update(home_facts)
 
-    def update_school_info(self, soup):
-        schools = soup.find('div', class_='ds-nearby-schools-list').contents
+    def update_school_info(self):
+        schools = self.soup.find('div', class_='ds-nearby-schools-list').contents
         school_ratings = [school.find('span', class_='ds-schools-display-rating').text for school in schools]
         self.info['Schools'] = '|'.join(school_ratings)
 
